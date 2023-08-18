80cd164294c210284c30df6cb2ef64237e620eec
Nhung NGUYEN <nnguyen@unowhy.com>
2023-08-18 14:28:05 +0200

    feature/test: first try on getting data from database

--- test.php
+++ test.php
@@ -1,3 +1,19 @@
 <?php
 
 echo "hello world";
+
+$connect = mysqli_connect('mydatabase', # service name (as in docker-compose.yml)
+'php_docker', # username of phpMyadmin
+'password', # password
+'camagru_database'); # db name in phpMyadmin
+
+$query = "SELECT * from php_docker_table";
+$result = mysqli_query($connect, $query);
+
+echo '<h1>MySQL content:</h1>';
+while($record = mysqli_fetch_assoc($result)){
+    echo '<h2>'.$record['title'].'</h2>';
+    echo '<p>'.$record['body'].'</p>';
+
+    echo 'Posted: '.$record['date_created'];
+} 
